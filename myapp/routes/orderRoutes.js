dc416f938b149af46168b6342728f67e3e374054
KerryPower <kerrycpower@gmail.com>
2024-07-11 18:22:15 +1200

    updated README.md

--- myapp/routes/orderRoutes.js
+++ myapp/routes/orderRoutes.js
@@ -8,12 +8,15 @@ const orderController = new OrderController(orderService);
 router.get("/", (req, res) => {
      orderController.getOrders(req, res);
 });
+
 router.post("/", (req, res) => {
     orderController.createOrder(req, res)
 });
+
 router.delete("/:id", (req, res) => {
     orderController.deleteOrder(req, res)
 });
+
 router.put("/:id", (req, res) => {
     orderController.updateOrder(req, res)
 });
